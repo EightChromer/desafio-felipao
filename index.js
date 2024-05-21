3916239a1e00d0d7ed03965d30d09f550d3f705d
EightChromer <vinifs935@gmail.com>
2024-05-21 15:34:00 -0300

    segue desafio do felipao

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-let nome = "vinicius"
-console.log("olaaa")
-console.log("testeeeee")
+let nomeDoHeroi = "Vinícius"
+let quantidadeDeXP = 8550
+let rankingDoHeroi = ""
+
+if (quantidadeDeXP < 1000) {
+    rankingDoHeroi = "Ferro"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (1001 <= quantidadeDeXP && quantidadeDeXP < 2000) {
+    rankingDoHeroi = "Bronze"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (2001 <= quantidadeDeXP && quantidadeDeXP < 5000) {
+    rankingDoHeroi = "Prata"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (5001 <= quantidadeDeXP && quantidadeDeXP < 7000) {
+    rankingDoHeroi = "Ouro"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (7001 <= quantidadeDeXP && quantidadeDeXP < 8000) {
+    rankingDoHeroi = "Platina"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (8001 <= quantidadeDeXP && quantidadeDeXP < 9000) {
+    rankingDoHeroi = "Ascendente"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (9001 <= quantidadeDeXP && quantidadeDeXP < 10000) {
+    rankingDoHeroi = "Imortal"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+else if (10001 <= quantidadeDeXP) {
+    rankingDoHeroi = "Radiante"
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + rankingDoHeroi)
+}
+
